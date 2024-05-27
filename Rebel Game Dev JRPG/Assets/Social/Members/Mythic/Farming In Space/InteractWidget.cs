7d132d2ac8d2c4a9de8c794b46aeb196594cd1f2
TeamMythic <mythicgaming234@gmail.com>
2024-05-27 15:30:39 -0700

    fix

--- Rebel Game Dev JRPG/Assets/Social/Members/Mythic/Farming In Space/InteractWidget.cs
+++ Rebel Game Dev JRPG/Assets/Social/Members/Mythic/Farming In Space/InteractWidget.cs
@@ -2,7 +2,10 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using RebelGameDevs.Utils.UnrealIntegration;
-public class InteractWidget : UIWidget
+namespace Mythic
 {
+	public class InteractWidget : UIWidget
+	{
 
+	}
 }
